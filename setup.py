442b3f147035d4d5fad68e91a5dab8a2944341d8
Dominic Hains <contact.dominichains@gmail.com>
2021-12-06 15:15:15 -0500

    chore: setup dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name="pxplore",
     version="0.1.0",
     packages=["pxplore"],
-    nstall_requires=[
-        'Click',
-        'requests'
+    install_requires=[
+        'click',
+        'requests',
+        'pandas'
     ],
     entry_points={"console_scripts": ["pxplore = pxplore.__main__:main"]},
 )
